e50b1379e18c6358bd3a8652c34eed1f6a6f5ef0
river <river226@gmail.com>
2015-04-09 21:30:59 -0500

    Flesh out exception

--- src/com/isensix/alerter/NotificationTray.java
+++ src/com/isensix/alerter/NotificationTray.java
@@ -9,71 +9,46 @@ import java.awt.PopupMenu;
 import java.awt.SystemTray;
 import java.awt.TrayIcon;
 
-
-
 // Local Imports
 import com.isensix.alerter.exceptions.*;
 import com.isensix.media.*;
 
 public class NotificationTray {
-	
+
 	// code adapted from: https://docs.oracle.com/javase/tutorial/uiswing/misc/systemtray.html
-	
+
 	 // Construct the System Tray
     public NotificationTray () throws NoTrayAccessException {
-    	if(test()) { 
-    		try {
-    			run();  // Builds the app
-    		} catch (AWTException e) {
-    			throw new NoTrayAccessException();
-    		}
-    	}
-    	else {
-    		throw new NoTrayAccessException();
-    	}
+    	if(test()) try { run();  // Builds the app
+			} catch (AWTException e) { throw new NoTrayAccessException("Exception Thrown\n"  + e); }
+    	else throw new NoTrayAccessException("No System Support");
     }
-    
-    
+
+
     private boolean test() { // Tests if System allows access to the System Tray
 		return SystemTray.isSupported();
 	}
 
 
 	public void run() throws AWTException {
- 
+
         final PopupMenu popup = new PopupMenu();
-        final TrayIcon trayIcon = new TrayIcon(createImage("images/bulb.gif", "tray icon"));
+        //final TrayIcon trayIcon = new TrayIcon(createImage("images/bulb.gif", "tray icon"));
         final SystemTray tray = SystemTray.getSystemTray();
-       
+
         // Create a pop-up menu components
-        MenuItem aboutItem = new MenuItem("About");
-        CheckboxMenuItem cb1 = new CheckboxMenuItem("Set auto size");
-        CheckboxMenuItem cb2 = new CheckboxMenuItem("Set tooltip");
-        Menu displayMenu = new Menu("Display");
-        MenuItem errorItem = new MenuItem("Error");
-        MenuItem warningItem = new MenuItem("Warning");
-        MenuItem infoItem = new MenuItem("Info");
-        MenuItem noneItem = new MenuItem("None");
-        MenuItem exitItem = new MenuItem("Exit");
-       
+        CheckboxMenuItem enabled = new CheckboxMenuItem("Alerts enabled");
+        MenuItem alerts = new MenuItem("Programmed Alerts");
+
+
         //Add components to pop-up menu
-        popup.add(aboutItem);
+        popup.add(enabled);
         popup.addSeparator();
-        popup.add(cb1);
-        popup.add(cb2);
-        popup.addSeparator();
-        popup.add(displayMenu);
-        displayMenu.add(errorItem);
-        displayMenu.add(warningItem);
-        displayMenu.add(infoItem);
-        displayMenu.add(noneItem);
-        popup.add(exitItem);
-       
+        displayMenu.add(Alerts);
+
         trayIcon.setPopupMenu(popup);
-       
-        
-       tray.add(trayIcon);
-        
+
+       	//tray.add(trayIcon); //Throw AWTException
     }
-	
+
 }
--- src/com/isensix/alerter/exceptions/NoTrayAccessException.java
+++ src/com/isensix/alerter/exceptions/NoTrayAccessException.java
@@ -3,8 +3,21 @@ package com.isensix.alerter.exceptions;
 public class NoTrayAccessException extends Exception {
 
 	/**
-	 * 
+	 * Keep Eclipse happy
 	 */
 	private static final long serialVersionUID = 1L;
 
+	String error = "Program was unable to access System Tray";
+
+	public NoTrayAccessException(){
+		// No input
+	}
+
+	public NoTrayAccessException(String input){
+		error += "\n" + input;
+	}
+
+	public String getException() {
+		return error;
+	}
 }
