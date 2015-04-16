f3dc6c434592eaa7083a84756a57b44a198520ef
river <river226@gmail.com>
2015-04-15 22:43:08 -0500

    Add AlertPopUp class

---
+++ com/isensix/alerter/AlertPopUp.java
@@ -0,0 +1,13 @@
+package com.isensix.alerter;
+
+public class AlertPopUp {
+  // TODO build up Alert pop up
+
+  public AlertPopUp() {
+
+  }
+
+  public void launchPopUp(String m) {
+      
+  }
+}
--- com/isensix/alerter/Backend.java
+++ com/isensix/alerter/Backend.java
@@ -1,5 +1,14 @@
 package com.isensix.alerter;
 
+import java.util.*;
+
 public class Backend {
   // TODO set up timer funtions
+  // Reference: http://stackoverflow.com/questions/6965296/running-a-java-method-at-a-set-time-each-day
+
+  public Backend() {
+
+  }
+
+  public void
 }
