06b73acb43276afd889c8882facdb710ef87129a
Jedidiah River Clemons-Johnson <river@Jedidiahs-Mac-mini.local>
2015-04-16 16:36:15 -0500

    Implement Private Classes

--- com/isensix/alerter/Backend.java
+++ com/isensix/alerter/Backend.java
@@ -2,13 +2,57 @@ package com.isensix.alerter;
 
 import java.util.*;
 
+@SuppressWarnings("unused")
 public class Backend {
-  // TODO set up timer funtions
-  // Reference: http://stackoverflow.com/questions/6965296/running-a-java-method-at-a-set-time-each-day
+	// TODO set up timer funtions
+	// Reference: http://stackoverflow.com/questions/6965296/running-a-java-method-at-a-set-time-each-day
+	ArrayList<AlertTimer> timers; 
 
-  public Backend() {
+	public Backend() {
+		timers = new ArrayList<AlertTimer>();
+	}
 
-  }
+	public void buildTimer(Alerts al) {
+		timers.add(new AlertTimer(al));
+	}
 
-  public void
+	private class AlertTimer extends TimerTask {
+		
+		public AlertTimer(){
+			
+		}
+		
+		public AlertTimer(Alerts al) {
+			Timer timer = new Timer();
+		    Calendar date = Calendar.getInstance();
+		    date.set(
+		      Calendar.DAY_OF_WEEK,
+		      Calendar.SUNDAY
+		    );
+		    date.set(Calendar.HOUR, 0);
+		    date.set(Calendar.MINUTE, 0);
+		    date.set(Calendar.SECOND, 0);
+		    date.set(Calendar.MILLISECOND, 0);
+		    // Schedule to run every Sunday in midnight
+		    // timer.schedule(new Timer(), date.getTime(),(1000 * 60 * 60 * 24 * 7));
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+
+	private class AlertPopUp {
+		// TODO build up Alert pop up
+
+		public AlertPopUp() {
+			
+		}
+
+		public void launchPopUp(String m) {
+
+		}
+	}
 }
