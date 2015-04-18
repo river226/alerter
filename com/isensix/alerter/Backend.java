63a993af5c02b780fc81ebbf56e88e3b00a1f77f
river <river226@gmail.com>
2015-04-17 21:21:53 -0500

    Work on Timer, some more

--- com/isensix/alerter/Backend.java
+++ com/isensix/alerter/Backend.java
@@ -1,6 +1,7 @@
 package com.isensix.alerter;
 
 import java.util.*;
+import javax.swing.Popup;
 
 @SuppressWarnings("unused")
 public class Backend {
@@ -18,58 +19,32 @@ public class Backend {
 
 	private class AlertTimer extends TimerTask {
 
-		public AlertTimer(){
+		private Calendar next;
+		private String message;
 
+		private AlertTimer(Calendar n, String m) {
+			next = n;
+			message = m;
 		}
 
 		public AlertTimer(Alerts al) {
 			ArrayList<Timer> timers = new ArrayList<Timer>();
 			ArrayList<Calendar> dates = new ArrayList<Calendar>();
 
-			if(al.isThisDay(0)) dates.add(setAlertMon(al.getTime()));
-			if(al.isThisDay(1)) dates.add(setAlertTues(al.getTime()));
-			if(al.isThisDay(2)) dates.add(setAlertWed(al.getTime()));
-			if(al.isThisDay(3)) dates.add(setAlertThurs(al.getTime()));
-			if(al.isThisDay(4)) dates.add(setAlertFri(al.getTime()));
-			if(al.isThisDay(5)) dates.add(setAlertSat(al.getTime()));
-			if(al.isThisDay(6)) dates.add(setAlertSun(al.getTime()));
+			if(al.isThisDay(0)) dates.add(this.setAlertDate(Calendar.MONDAY, al.getTime()));
+			if(al.isThisDay(1)) dates.add(this.setAlertDate(Calendar.TUESDAY, al.getTime()));
+			if(al.isThisDay(2)) dates.add(this.setAlertDate(Calendar.WEDNESDAY, al.getTime()));
+			if(al.isThisDay(3)) dates.add(this.setAlertDate(Calendar.THURSDAY, al.getTime()));
+			if(al.isThisDay(4)) dates.add(this.setAlertDate(Calendar.FRIDAY, al.getTime()));
+			if(al.isThisDay(5)) dates.add(this.setAlertDate(Calendar.SATURDAY, al.getTime()));
+			if(al.isThisDay(6)) dates.add(this.setAlertDate(Calendar.SUNDAY, al.getTime()));
 
 			for(int i = 0; i < dates.length(); i++)
-		  	timers.add(Timer.schedule(new AlertTimer(), dates.get(i).getTime()));
+		  	timers.add(Timer.schedule(new AlertTimer(dates.get(i) al.getMessage()), dates.get(i).getTime()));
 		}
 
-		private Calendar setAlertMon(int t) {
-			Calendar date = date.set(Calendar.DAY_OF_WEEK,Calendar.MONDAY);
-			return setTime(date, t);
-		}
-
-		private Calendar setAlertTues(int t) {
-			Calendar date = date.set(Calendar.DAY_OF_WEEK,Calendar.TUESDAY);
-			return setTime(date, t);
-		}
-
-		private Calendar setAlertWed(int t) {
-			Calendar date = date.set(Calendar.DAY_OF_WEEK,Calendar.WEDNESDAY);
-			return setTime(date, t);
-		}
-
-		private Calendar setAlertThurs(int t) {
-			Calendar date = date.set(Calendar.DAY_OF_WEEK,Calendar.THURSDAY);
-			return setTime(date, t);
-		}
-
-		private Calendar setAlertFri(int t) {
-			Calendar date = date.set(Calendar.DAY_OF_WEEK,Calendar.FRIDAY);
-			return setTime(date, t);
-		}
-
-		private Calendar setAlertSat(int t) {
-			Calendar date = date.set(Calendar.DAY_OF_WEEK,Calendar.SATURDAY);
-			return setTime(date, t);
-		}
-
-		private Calendar setAlertSun(int t) {
-			Calendar date = date.set(Calendar.DAY_OF_WEEK,Calendar.SUNDAY);
+		private Calendar setAlertDate(int day, int time) {
+			Calendar date = date.set(Calendar.DAY_OF_WEEK,day);
 			return setTime(date, t);
 		}
 
@@ -86,7 +61,7 @@ public class Backend {
 		}
 	}
 
-	private class AlertPopUp {
+	private class AlertPopUp extends Popup {
 		// TODO build up Alert pop up
 
 		public AlertPopUp() {
