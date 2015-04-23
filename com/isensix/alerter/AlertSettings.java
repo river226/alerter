70f59eebd5b445536182ecee0b027f5a1d7a1129
river <river226@gmail.com>
2015-04-22 21:36:20 -0500

    Start on Settings Panel

--- com/isensix/alerter/AlertSettings.java
+++ com/isensix/alerter/AlertSettings.java
@@ -1,15 +1,28 @@
 package com.isensix.alerter;
 
+/**
+ * This class builds a basic GUI with a List area listing all current alerts
+ * and a config area that is used to edit and add alerts. 
+ */
+
 import java.awt.EventQueue;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 
+import java.util.ArrayList;
+
+import javax.swing.event;
+import javax.swing.event.ListSelectionEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JList;
 
 @SuppressWarnings("unused")
 public class AlertSettings {
 
 	private JFrame frame;
+	private JList<String> list;
+	private ArrayList<Alerts> alerts;
 
 	/**
 	 * Launch the application.
@@ -30,7 +43,8 @@ public class AlertSettings {
 	/**
 	 * Create the application.
 	 */
-	public AlertSettings() {
+	public AlertSettings(ArrayList<Alerts> al) {
+		alerts = al;
 		initialize();
 	}
 
@@ -51,6 +65,29 @@ public class AlertSettings {
 		 	winWidth, winHeight); // size windowIa
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		this.buildList();
+
+	}
+
+	private void buildList() {
+		list = new JList<String>();
+		list.addListSelectionListener(new ListSelectionListiner(){
+			public valueChanged(ListSelectionEvent e) {
+				changeConfig();
+			}
+		});
+	}
+
+	private void changeConfig() {
+		// TODO Change config area to match selected alert
+	}
+
+	private void builtConfig() {
+		// TODO Layout the config area
+	}
+
+	private void addAlert() {
+		// TODO import data from config area to create a new alert, and add to list
 	}
 
 }
