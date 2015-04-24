8a7cfb316529dca25c97c25321fd396611c31516
river <river226@gmail.com>
2015-04-23 21:43:52 -0500

    Add Variables

--- com/isensix/alerter/AlertSettings.java
+++ com/isensix/alerter/AlertSettings.java
@@ -2,12 +2,13 @@ package com.isensix.alerter;
 
 /**
  * This class builds a basic GUI with a List area listing all current alerts
- * and a config area that is used to edit and add alerts. 
+ * and a config area that is used to edit and add alerts.
  */
 
 import java.awt.EventQueue;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.Checkbox;
 
 import java.util.ArrayList;
 
@@ -16,13 +17,32 @@ import javax.swing.event.ListSelectionEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
 
 @SuppressWarnings("unused")
 public class AlertSettings {
 
+	// Define classes that can change
 	private JFrame frame;
 	private JList<String> list;
 	private ArrayList<Alerts> alerts;
+	private GroupLayout layout;
+	private Checkbox[] dayCheck;
+	private JButton add;
+	private JTextField messageBox;
+	private JTextField timeBox;
+
+	// Define final values
+	private final JLabel aLabel = new JLabel("Alerts");
+	private final JLabel mLabel = new JLabel("Messages");
+	private final JLabel[] dayLabel = [new JLabel("M"),
+		new JLabel("Tu"), new JLabel("W"), new JLabel("Tr"),
+		new JLabel("F"), new JLabel("Sa"), new JLabel("Su")];
+	private final JLabel[] timeLabel = [new JLabel("Time"),
+		new JLabel("am"), new JLabel("pm"), new JLabel("24hr")];
 
 	/**
 	 * Launch the application.
@@ -76,6 +96,11 @@ public class AlertSettings {
 				changeConfig();
 			}
 		});
+	}
+
+	private GridLayout buildGridLayout() {
+		// TODO Build the layout
+		return null;
 	}
 
 	private void changeConfig() {
